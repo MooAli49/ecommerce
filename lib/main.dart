3c07e2e20d7d91b90d6cb11f76edf016dfb7d86e
Mohamed Ali <ma8510007@gmail.com>
2025-06-13 20:49:29 +0300

    initialize Firebase in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:ecommerce/ecommerce_app.dart';
+import 'package:ecommerce/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase with the default options for the current platform
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ECommerceApp());
 }
