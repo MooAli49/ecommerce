5bdba9494eb7485a6a9117e8cd84cca35c00731c
Mohamed Ali <ma8510007@gmail.com>
2025-07-23 12:56:12 +0300

    enhance ui : added svg picture for empty cart state

--- lib/feature/cart/ui/screens/cart_screen.dart
+++ lib/feature/cart/ui/screens/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../controller/cubit/cart_cubit.dart';
 import '../widgets/cart_item_row.dart';
@@ -21,23 +22,25 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<CartCubit, CartState>(
-        builder: (context, state) {
-          if (state is CartLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is CartError) {
-            return Center(child: Text(state.message));
-          } else if (state is CartLoaded) {
-            final products = state.products;
-            if (products.isEmpty) {
-              return const Center(child: Text('Your cart is empty.'));
-            }
-            double total = products.fold(
-              0,
-              (sum, item) => sum + (item.price * item.quantity),
-            );
-            return Column(
+    return BlocBuilder<CartCubit, CartState>(
+      builder: (context, state) {
+        if (state is CartLoading) {
+          return Scaffold(
+            body: const Center(child: CircularProgressIndicator()),
+          );
+        } else if (state is CartError) {
+          return Scaffold(body: Center(child: Text(state.message)));
+        } else if (state is CartLoaded) {
+          final products = state.products;
+          if (products.isEmpty) {
+            return Scaffold(body: _showEmptyCartImage());
+          }
+          double total = products.fold(
+            0,
+            (sum, item) => sum + (item.price * item.quantity),
+          );
+          return Scaffold(
+            body: Column(
               children: [
                 Expanded(
                   child: ListView.builder(
@@ -52,11 +55,29 @@ class _CartScreenState extends State<CartScreen> {
                 ),
                 CartTotalFooter(total: total),
               ],
-            );
-          }
-          return const SizedBox.shrink();
-        },
-      ),
+            ),
+          );
+        }
+        return Scaffold(body: const SizedBox.shrink());
+      },
+    );
+  }
+
+  Widget _showEmptyCartImage() {
+    print('safaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      spacing: 20,
+      children: [
+        Center(
+          child: SvgPicture.asset(
+            'assets/images/cart_empty.svg',
+            width: 200,
+            height: 200,
+          ),
+        ),
+        const Center(child: Text('Your cart is empty.')),
+      ],
     );
   }
 
